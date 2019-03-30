e128b64e8e326663a59fcca1d5c0be6a0dd383e1
Dell <olgaigpavlenko@gmail.com>
2019-03-30 14:28:03 +0200

    add Task2, Task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,28 +10,30 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Add Radius");
+            /* Console.WriteLine("Add Radius");
 
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
+             string radiusString = Console.ReadLine();
+             int.TryParse(radiusString, out int radius);
 
-            Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");
+             Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");  */
 
             //Test1();
-            
+            //Test2();
+            Test3();
+
             Console.ReadKey();
 
             
         }
 
-        public static decimal PerformCalculation(int r)
+       /* public static decimal PerformCalculation(int r)
         {
             const decimal Pi = 3.14m;
 
             decimal square = Pi * r * r;
 
             return square;
-        }
+        }*/
 
         public static void Test1()
         {
@@ -45,7 +47,24 @@ namespace Task1
             var myVariab1le = 5;
         }
 
-       
+        public static void Test2()
+        {
+            const double Pi = 3.141592653;
+            const double E = 2.7182818284590452;
+            Console.WriteLine(Pi);
+            Console.WriteLine(E);
+
+        }
+
+        public static void Test3()
+        {
+            string str1 = "\nмоя строка 1";
+            string str2 = "\tмоя строка 2";
+            string str3 = "\aмоя строка 3";
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
 
     }
 }
