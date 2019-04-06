f2290d471031306706549bbf048549801f703aad
Dell <olgaigpavlenko@gmail.com>
2019-04-06 11:40:32 +0300

    added Task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -18,7 +18,8 @@ namespace Task1
              Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");*/
 
             //Task1();
-            Task2();
+            //Task2();
+            Task3();
             Console.ReadKey();
         }
 
@@ -50,6 +51,23 @@ namespace Task1
             }
             else
                 Console.WriteLine("Condition is false");
+        }
+
+        public static void Task3()
+        {
+            Console.WriteLine("Enter the number");
+            string numberForLength = Console.ReadLine();
+            int number = Convert.ToInt32(numberForLength);
+            int lenght = numberForLength.Length;
+            if (lenght == 3)
+            {
+                if (number % 2 != 0)
+                    Console.WriteLine("number!");
+                else
+                    Console.WriteLine("Condition false");
+            }
+            else
+                Console.WriteLine("Condition false");
         }
     }
 }
