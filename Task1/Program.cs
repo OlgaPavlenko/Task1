a5c47eb83407560af27cd26595dab6c65a96386b
Dell <olgaigpavlenko@gmail.com>
2019-04-06 11:26:27 +0300

    added Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,14 +10,15 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-           /* Console.WriteLine("Add Radius");
+            /* Console.WriteLine("Add Radius");
 
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
+             string radiusString = Console.ReadLine();
+             int.TryParse(radiusString, out int radius);
 
-            Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");*/
+             Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");*/
 
-            Task1();
+            //Task1();
+            Task2();
             Console.ReadKey();
         }
 
@@ -37,5 +38,18 @@ namespace Task1
                 int hour = sec / 3600;
                 Console.WriteLine(hour);
             }
+        public static void Task2()
+        {
+            int a = 12;
+            int b = 16;
+            int c = 35;
+
+            if ((a <= b && b <= c) && a < c)
+            {
+                Console.WriteLine("Number b between a and c");
+            }
+            else
+                Console.WriteLine("Condition is false");
+        }
     }
 }
