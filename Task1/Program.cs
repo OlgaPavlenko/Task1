a42fba3ca6e79c2c0412868dd9f8315c674c8a0d
Dell <olgaigpavlenko@gmail.com>
2019-04-16 16:36:31 +0300

    added UnpackingArray

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,12 +10,13 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Add Radius");
+            //Console.WriteLine("Add Radius");
 
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
+            //string radiusString = Console.ReadLine();
+            //int.TryParse(radiusString, out int radius);
 
-            Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");
+            //Console.WriteLine($"The square of the round {Program.PerformCalculation(15)}");
+            UnpackingArray();
             Console.ReadKey();
         }
 
@@ -27,5 +28,45 @@ namespace Task1
 
             return square;
         }
+
+        public static void UnpackingArray()
+        {
+            int[] array = {0, 2, 3, 5, 6 };
+            string[] resArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i]; j++)
+                {
+                    string nuls = string.Empty;
+                    string ones = string.Empty;
+
+                    if (array[0] == 0 && i == 0)
+                    {
+                        continue;
+                    }
+                    {
+                        if (i % 2 == 0)
+                        {
+                            nuls += "0";
+                            Console.Write(nuls);
+                            continue;
+                        }
+
+                        if (i % 2 != 0)
+                        {
+                                ones += "1";
+                                Console.Write(ones);
+                                continue;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
