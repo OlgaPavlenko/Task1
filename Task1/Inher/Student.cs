2afb82fe9cfa86fa5e101ba5431fc58316aa288f
Dell <olgaigpavlenko@gmail.com>
2019-05-07 19:58:45 +0300

    added Student

---
+++ Task1/Inher/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task1.Inher
+{
+    class Student : User
+    {
+        private int stipendiya;
+        private string course;
+
+        public int Stipendiya
+        {
+            get
+            {
+                return stipendiya;
+            }
+            set
+            {
+                stipendiya = value;
+            }
+        }
+
+        public string Course
+        {
+            get
+            {
+                return course;
+            }
+            set
+            {
+                course = value;
+            }
+        }
+    }
+}
