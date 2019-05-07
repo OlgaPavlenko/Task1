e8ee8e4b25e1fab50971146598e5ad169875aaff
Dell <olgaigpavlenko@gmail.com>
2019-05-07 20:23:56 +0300

    added Driver

---
+++ Task1/Inher/Driver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Inher
+{
+    class Driver : Worker
+    {
+        private int stage;
+        private Category category;
+
+        public int Stage
+        {
+            get
+            {
+                return stage;
+            }
+            set
+            {
+                stage = value;
+            }
+        }
+
+        public Driver(int stage, Category category, string name, int age, decimal salary)
+            : base(name, age, salary)
+        {
+            Stage = stage;
+            this.category = category;
+
+        }
+    }
+
+    public enum Category
+    {
+        A,
+        B,
+        C
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -11,11 +11,14 @@ namespace Task1.Inher
     {
         static void Main()
         {
-            Employee worker = new Employee("Ivan", 25, 1000);
-            Employee worker2 = new Employee("Vasya", 26, 2000);
+            //Employee worker = new Employee("Ivan", 25, 1000);
+            //Employee worker2 = new Employee("Vasya", 26, 2000);
 
-            Console.WriteLine(worker.Age + worker2.Age);
-            Console.WriteLine(worker.Salary + worker2.Salary);
+            //Console.WriteLine(worker.Age + worker2.Age);
+            //Console.WriteLine(worker.Salary + worker2.Salary);
+
+            Driver driver = new Driver(12, Category.A, "Ivan", 25, 1000);
+            Console.WriteLine(driver.Stage);
         }
 
         
